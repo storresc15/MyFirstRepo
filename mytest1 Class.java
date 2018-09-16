88535c838112ca6dbe0a98e3eff99ca3e5016780
Sebastian Torres <sebas.torres.88@gmail.com>
2018-09-16 14:00:50 -0500

    Commit Test

---
+++ mytest1 Class.java
@@ -0,0 +1,18 @@
+public class myTest1 {
+
+    public static void main(String args[]){
+
+        myTest1 test1 = new myTest1();
+        test1.displaytext1("Sebas");
+
+    }
+
+    public String displaytext1 (String str){
+
+        String result = "";
+        result = "Thanks "+str+" "+"for completing the first test";
+        return result;
+
+    }
+
+}
